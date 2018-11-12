ba653c8615ec9af6cc0c49193709853292cd1195
关彬 <guanbin@guanbin.local>
2018-11-12 18:47:43 +0800

    修改被邀请第二次进入状态

--- pages/employee/join-company/confirmCompanyInformation/index.js
+++ pages/employee/join-company/confirmCompanyInformation/index.js
@@ -11,49 +11,77 @@ Page({
     var _this = this;
     //检测登陆
     if (!(app.checkSession()) || wx.getStorageSync('open_id') == '' || wx.getStorageSync('xy_session') == '') {
-      app.checkLogin().then(function (res) { })
+      app.checkLogin().then(function(res) {})
     }
-    if (!options.company_code){
+    if (!options.company_code) {
       _this.data.company_code = decodeURIComponent(options.scene);
-    }else{
+    } else {
       _this.data.company_code = options.company_code;
     }
     app.Util.network.POST({
       url: app.globalData.BASE_API_URL,
       params: {
         service: 'company',
-        method: 'get_company_info',
+        method: 'get_review_status',
         data: JSON.stringify({
-          union_id: wx.getStorageSync('xy_session'),
-          company_code: _this.data.company_code
+          union_id: wx.getStorageSync('xy_session')
         })
       },
       success: res => {
         console.log(res);
+        var resdata = res.data.result;
         if (res.data.sub_code == 0) {
-          console.log('数据成功');
-          _this.setData({
-            cd: res.data.result
-          })
+          if (resdata.employee_status === 2) {
+            wx.reLaunch({
+              url: '../../../manage/manage',
+            })
+          } else {
+            app.Util.network.POST({
+              url: app.globalData.BASE_API_URL,
+              params: {
+                service: 'company',
+                method: 'get_company_info',
+                data: JSON.stringify({
+                  union_id: wx.getStorageSync('xy_session'),
+                  company_code: _this.data.company_code
+                })
+              },
+              success: res => {
+                console.log(res);
+                if (res.data.sub_code == 0) {
+                  console.log('数据成功');
+                  _this.setData({
+                    cd: res.data.result
+                  })
+                } else {
+                  console.log(res.data.sub_msg);
+                  toast.showToast(this, {
+                    toastStyle: 'toast',
+                    title: res.data.sub_msg,
+                    duration: 1000,
+                    mask: false,
+                    cb: function() {
+                      wx.reLaunch({
+                        url: '../choiceJoin/index',
+                      })
+                    }
+                  });
+                }
+              },
+              fail: res => {
+                console.log('fail');
+              }
+            })
+          }
         } else {
           console.log(res.data.sub_msg);
-          toast.showToast(this, {
-            toastStyle: 'toast',
-            title: res.data.sub_msg,
-            duration: 1000,
-            mask: false,
-            cb: function() {
-              wx.reLaunch({
-                url: '../choiceJoin/index',
-              })
-            }
-          });
         }
       },
       fail: res => {
         console.log('fail');
       }
     })
+
   },
   next: function() {
     var _this = this;
