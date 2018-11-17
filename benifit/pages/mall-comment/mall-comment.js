7841137f28ac565ec71b9a158015aa460cd18f8c
fantingsheng <fants0230@sina.com>
2018-11-17 17:16:00 +0800

    add collect-info guide page

---
+++ benifit/pages/mall-comment/mall-comment.js
@@ -0,0 +1,248 @@
+// pages/mall-comment/mall-comment.js
+var md5 = require('../../../utils/md5.js');
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    commerce_id: null,
+    selectedImages: [],
+    uploading: false,
+    uploadImagesLimit: 9,
+    rate: -1,
+    content: null,
+    formReady: false,
+    disabled: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.commerce_id);
+    var self = this;
+    self.setData({
+      commerce_id: options.commerce_id
+    })
+    
+    if (!(app.checkSession())) {
+      app.checkLogin().then(function (res) {
+        console.log();
+      })
+    } else {
+      
+    }
+  },
+
+  // 图片选择
+  selectImage() {
+    if (this.data.uploading) {
+      return;
+    }
+    wx.chooseImage({
+      count: this.data.uploadImagesLimit - this.data.selectedImages.length,
+      sizeType: 'compressed',
+      success: res => {
+        for (var i in res.tempFilePaths) {
+          this.data.selectedImages.push(res.tempFilePaths[i]);
+        }
+        this.setData({
+          selectedImages: this.data.selectedImages
+        });
+      }
+    })
+  },
+
+  /**
+   * 监听表单提交状态
+   */
+  monitorInput: function (e) {
+    if (e.detail.value !== '') {
+      this.setData({
+        formReady: true
+      });
+    }
+    if (this.data.formReady && this.data.rate !== -1) {
+      this.setData({
+        disabled: false
+      });
+    }
+  },
+
+  // 评星点击
+  rateChange(e) {
+    var rate = e.currentTarget.dataset.rate;
+    this.setData({
+      rate: rate
+    });
+    if (this.data.formReady && this.data.rate !== -1) {
+      this.setData({
+        disabled: false
+      });
+    }
+  },
+
+  // 全屏看图
+  previewImage(e) {
+    var index = e.currentTarget.dataset.index;
+    wx.previewImage({
+      current: this.data.selectedImages[index],
+      urls: this.data.selectedImages
+    })
+  },
+
+  // 移除图片
+  removeImage(e) {
+    var index = e.currentTarget.dataset.index;
+    this.data.selectedImages.splice(index, 1);
+    this.setData({
+      selectedImages: this.data.selectedImages
+    });
+  },
+
+  // 提交评价
+  reviewSubmit(e) {
+    if (this.data.uploading) {
+      return;
+    }
+    if (this.data.rate == -1) {
+      wx.showToast({
+        icon: 'none',
+        title: '请选择评星'
+      })
+      return;
+    }
+    if (e.detail.value.content.length == 0) {
+      wx.showToast({
+        title: '请输入评价内容'
+      })
+      return;
+    }
+    this.setData({ content: e.detail.value.content });
+    if (this.data.selectedImages.length > 0) {
+      this.uploadImage(0);
+    } else {
+      this.submit(e.detail.value.content);
+    }
+  },
+
+  // 提交
+  submit() {
+    var that = this;
+    console.log(that.data.selectedImages);
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'commerce',
+        method: 'add_comment',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({
+          commerce_id: that.data.commerce_id,
+          user_nickname: wx.getStorageSync('nickname'),
+          user_avatar_url: wx.getStorageSync('avatar'),
+          pic_url: that.data.selectedImages,
+          comment: that.data.content,
+          score: that.data.rate + 1
+        })
+      },
+      success: data => {
+        that.reviewDone();
+      },
+      fail: data => {
+        wx.showToast({
+          icon: 'none',
+          title: data.message
+        })
+      }
+    });
+    
+  },
+
+  // 上传图片
+  uploadImage(i) {
+    var that = this;
+    var service = 'commerce';
+    var data = JSON.stringify({});
+    var method = 'upload_comment_file';
+    var app_id = '65effd5a42fd1870b2c7c5343640e9a8';
+    var timestamp = Math.round(new Date().getTime() / 1000 - 28800);
+    var sign_type = 'MD5';
+    var stringA = 'app_id=' + app_id + '&data=' + data + '&method=' + method + '&service=' + service + '&timestamp=' + timestamp;
+    var sign = md5.hex_md5(stringA + '&key=a8bfb7a5f749211df4446833414f8f95');
+    that.setData({
+      uploading: true
+    });
+    var uploadTask = wx.uploadFile({
+      url: app.globalData.BENIFIT_API_URL,
+      filePath: that.data.selectedImages[i],
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+      name: 'comment_pic',
+      formData: {
+        union_id: wx.getStorageSync('xy_session'),
+        service: service,
+        method: method,
+        app_id: app_id,
+        timestamp: timestamp,
+        sign_type: sign_type,
+        sign: sign,
+        data: data
+      },
+      success: res => {
+        var data = JSON.parse(res.data);
+        that.setData({
+          [`selectedImages[${i}]`]: data.result.comment_pic
+        })
+        if (i < that.data.selectedImages.length - 1) {
+          i++;
+          that.uploadImage(i);
+        } else {
+          that.submit(that.data.content);
+        }
+      },
+      fail: res => {
+        wx.showToast({
+          title: '上传失败'
+        })
+        that.setData({
+          ['progress[' + i + ']']: false
+        });
+      }
+    })
+    uploadTask.onProgressUpdate(res => {
+      that.setData({
+        ['progress[' + i + ']']: res.progress
+      });
+    })
+  },
+
+  // 评价完成
+  reviewDone() {
+    wx.showToast({
+      title: '提交成功',
+      success: () => {
+        setTimeout(() => {
+          wx.navigateBack();
+        }, 1500)
+      }
+    })
+  },
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  }
+
+})
---
+++ benifit/pages/mall-detail/mall-detail.js
@@ -0,0 +1,365 @@
+// pages/mall-detail/mall-detail.js
+const app = getApp();
+var QQMapWX = require('../../../utils/qqmap-wx-jssdk.min.js');
+var wxParse = require('../../vendor/wxParse/wxParse.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    slide_data: {
+      thumbnail_url: [],
+      imgCount: 0,
+      indicatorDots: false,
+      autoplay: false,
+      interval: 20000,
+      duration: 300
+    },
+    showVipCard: false,
+    is_vip: false,
+    commerce_id: null,
+    commerce_type: null,
+    commerceDetail: null,
+    employeeInfo: null,
+    protocolInfo: null,
+    commentList: null,
+    rate: 2,
+    dialog_discount: null,
+    dialog_discount_limit: null,
+    systemInfo: null,
+    viewID: "discSec",
+    businessStatus: "",
+    latitude: null,
+    longitude: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this; 
+    that.setData({
+      commerce_id: options.commerce_id,
+      commerce_type: options.commerce_type
+    })
+    if (!(app.checkSession())) {
+      app.checkLogin().then(function (res) {
+        that.getEmployeeInfo();
+        that.getDetailInfo(that.data.commerce_id);
+      })
+    } else {
+      that.getDetailInfo(that.data.commerce_id);
+      that.getEmployeeInfo();
+    }
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          systemInfo: res
+        })
+      }
+    })
+  
+  },
+
+  /**
+   * 获取员工信息
+   */
+  getEmployeeInfo() {
+    var that = this;
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'company',
+        method: 'get_employee_info',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({})
+      },
+      success: res => {
+        //console.log(res.data.result);
+        if (res.data.result) {
+          if (res.data.result.has_employee_benefit == 1) {
+            that.setData({
+              is_vip: true,
+              employeeInfo: res.data.result
+            })
+          }
+          that.setData({
+            employeeInfo: res.data.result
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取商家详情
+   */
+  getDetailInfo(commerce_id) {
+    var that = this;
+    var slide_img = "slide_data.thumbnail_url";
+    var imgCount = "slide_data.imgCount";
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'commerce',
+        method: 'get_commerce_info',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({
+          commerce_id: commerce_id
+        })
+      },
+      success: res => {
+        if (res.data.result) {
+          if (res.data.result.details) {
+            wxParse.wxParse('details', 'html', res.data.result.details, that, 5);
+          }
+          that.setData({
+            commerceDetail: res.data.result,
+            [slide_img]: res.data.result.thumbnail_url,
+            [imgCount]: res.data.result.thumbnail_url.length
+          })
+          wx.setNavigationBarTitle({
+            title: res.data.result.name
+          })
+          that.onBusiness(res.data.result.business_hours);
+        }
+        that.getProtocol(commerce_id, that.data.commerce_type);
+        that.getComments(commerce_id);
+        app.Util.generateMap(this, res.data.result.address);
+      }
+    })
+  },
+  
+  /**
+  * 用来判断一个时间是不是在某个时间段内
+  * beginTime 开始时间 
+  * endTime 结束时间 
+  */
+  onBusiness(businessHours) {
+    if (businessHours == null) {
+      this.setData({
+        businessStatus: "营业中"
+      })
+      return ;
+    }
+    for (var i=0; i<businessHours.length; i++) {
+      var hour = new Date().getHours();
+      var min = new Date().getMinutes();
+
+      var varTime = hour + ':' + min;
+      
+      var strb = businessHours[i].start.split(":");
+      if (strb.length != 2) {
+        return false;
+      }
+      var stre = businessHours[i].end.split(":");
+      if (stre.length != 2) {
+        return false;
+      }
+      var strv = varTime.split(":");
+      if (strv.length != 2) {
+        return false;
+      }
+      var b = new Date();
+      var e = new Date();
+      var v = new Date();
+      b.setHours(strb[0]);
+      b.setMinutes(strb[1]);
+      e.setHours(stre[0]);
+      e.setMinutes(stre[1]);
+      v.setHours(strv[0]);
+      v.setMinutes(strv[1]);
+      
+      if ((v.getTime() - b.getTime() >= 0 && (e.getTime() - v.getTime()) >= 0)) {
+        this.setData({
+          businessStatus: "营业中"
+        })
+        return false;
+      } else {
+        this.setData({
+          businessStatus: "已歇业"
+        })
+      }
+    }
+    
+  },
+
+
+  /**
+   * 获取商家优惠协议
+   */
+  getProtocol(commerce_id, typeid) {
+    var that = this;
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'commerce',
+        method: 'get_commerce_discount',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({
+          commerce_id: commerce_id,
+          type: typeid
+        })
+      },
+      success: res => {
+        if (res.data.result) {
+          var data = res.data.result;
+          for (var i = 0; i < data.length; i++) {
+            data[i].deal_price_fen = String(data[i].deal_price_fen).split('');
+          }
+          that.setData({
+            protocolInfo: data
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取评论信息
+   */
+  getComments(commerce_id) {
+    var that = this;
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'commerce',
+        method: 'get_comment_list',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({
+          commerce_id: commerce_id
+        })
+      },
+      success: res => {
+        if (res.data.result) {
+          that.setData({
+            commentList: res.data.result
+          })
+        }
+      }
+    })
+  },
+
+  gotoView: function(e) {
+    this.setData({
+      viewID: e.currentTarget.dataset.partid
+    })
+  },
+
+  /**
+   * 关闭VIP卡弹窗
+   */
+  closeDialog: function () {
+    this.setData({
+      showVipCard: false
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#ffffff',
+    })
+  },
+
+  /**
+   * 协议买单
+   */
+  cardTopay: function (e) {
+    if (this.data.is_vip == false) {
+      wx.navigateTo({
+        url: '/benifit/pages/vip-card/vip-card'
+      })
+      return ;
+    }
+    this.setData({
+      showVipCard: true,
+      dialog_discount: e.currentTarget.dataset.discount,
+      dialog_discount_limit: e.currentTarget.dataset.limit,
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#000000',
+    })
+  },
+
+  /**
+   * 查看幻灯片
+   */
+  viewPhoto: function (e) {
+    var index = e.currentTarget.dataset.index;
+    wx.previewImage({
+      current: this.data.slide_data.thumbnail_url[index],
+      urls: this.data.slide_data.thumbnail_url
+    })
+  },
+
+  // 全屏查看评论图
+  previewImage(e) {
+    var pindex = e.currentTarget.dataset.pindex;
+    var mindex = e.currentTarget.dataset.mindex;
+    console.log(this.data.commentList);
+    wx.previewImage({
+      current: this.data.commentList[mindex].pic_url[pindex],
+      urls: this.data.commentList[mindex].pic_url
+    })
+  },
+
+  // 查看房型缩略图
+  previewHotelImage(e) {
+    var pindex = e.currentTarget.dataset.pindex;
+    var row = e.currentTarget.dataset.rowid;
+    wx.previewImage({
+      current: this.data.protocolInfo[row].thumbnail_url[pindex],
+      urls: this.data.protocolInfo[row].thumbnail_url
+    })
+  },
+
+  /**
+   * 拨打电话
+   */
+  makePhoneCall: function (e) {
+    wx.makePhoneCall({
+      phoneNumber: e.currentTarget.dataset.phone
+    })
+  },
+
+  /**
+   * 打开地图服务
+   */
+
+  openMap: function () { 
+    wx.openLocation({
+      latitude: this.data.latitude,
+      longitude: this.data.longitude,
+      scale: 28
+    })    
+  },
+
+  /**
+   * 去评论
+   */
+  enterComment: function (e) {
+    if (this.data.is_vip) {
+      var commerce_id = e.currentTarget.dataset.commerceid;
+      wx.navigateTo({
+        url: '/benifit/pages/mall-comment/mall-comment?commerce_id=' + commerce_id
+      })
+    } else {
+      wx.showToast({
+        icon: 'none',
+        title: '你还不是VIP，不能评论'
+      })
+    }
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getComments(this.data.commerce_id);
+  }
+
+
+
+})
