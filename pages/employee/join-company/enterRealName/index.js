98870fc412742a5312d4cbb720b47e1077c62bbe
fantingsheng <fants0230@sina.com>
2018-12-05 10:33:37 +0800

    修改webview环境地址错误

--- pages/employee/join-company/enterRealName/index.js
+++ pages/employee/join-company/enterRealName/index.js
@@ -2,6 +2,7 @@ var app = getApp();
 var toast = require('../../../../templates/showToast/showToast');
 Page({
   data: {
+    web_url: app.globalData.WEB_VIEW_URL,
     mainTitle: '输入真实姓名',
     button_text: '提交',
     codevalue: '',
