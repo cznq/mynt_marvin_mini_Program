0e4a3f2ca66553d1ed593980b982fcc7e94683f9
关彬 <guanbin@guanbin.local>
2018-12-18 16:36:54 +0800

    优化

--- templates/companyPages/companyPages.js
+++ templates/companyPages/companyPages.js
@@ -24,7 +24,7 @@ function pubData(_this, app, service, method, parameter, cb) {
         //   res.data.result.introduction = res.data.result.introduction.substr(0, 68) + '...';
         //   res.data.result.introductionAll_button = true;
         // }
-        
+        //res.data.result.introduction = '复古的风格发的';
         res.data.result.isRobotReview = false;
         _this.setData({
           cd: res.data.result
@@ -80,7 +80,8 @@ function introductionSwitch(_this){
     var sonHeight = qry.height;
     if (sonHeight >= 88) {
       _this.setData({
-        "cd.isEllipsis": true
+        "cd.isEllipsis": true,
+        "cd.isEllipsisbutton":true
       })
     }
   }).exec();
