7e4a79f367630984349e20c1cd8ded21588509d7
关彬 <guanbin@guanbin.local>
2018-12-20 17:42:13 +0800

    更改输入真实姓名webview传参方式

--- pages/create-company/enterRealName/index.js
+++ pages/create-company/enterRealName/index.js
@@ -9,15 +9,15 @@ Page({
     CstateCode:1,//1为创建 2为编辑
     web_url: app.globalData.WEB_VIEW_URL,
     company_code:null,
-    company_verify_code:null
+    company_verify_code:null,
+    view_web_url:''
   },
   onLoad: function(options) {
     var _this = this;
-    //_this.data.company_verify_code = options.company_verify_code;
+    var view_web_url = _this.data.web_url + '/admin/become?company_code=' + options.company_verify_code + ' &CstateCode=' + _this.data.CstateCode;
+    console.log(view_web_url);
     _this.setData({
-      company_code: options.company_verify_code,
-      CstateCode: _this.data.CstateCode
+      view_web_url: view_web_url
     })
-
   }
 })
