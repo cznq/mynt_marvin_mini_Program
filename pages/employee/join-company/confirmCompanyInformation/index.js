ac2a24f09603d534aec9d02a161eb83de16fb83c
suten <sutenwang@slightech.com>
2019-01-04 16:25:02 +0800

    style(confirm join info): add apply-info with no logo
    add apply-info with no logo

--- pages/employee/join-company/confirmCompanyInformation/index.js
+++ pages/employee/join-company/confirmCompanyInformation/index.js
@@ -7,7 +7,7 @@ Page({
         hint: '二维码与邀请码来自于企业内部人员的分享,可\n向企业员工或管理员索要',
         company_code: '',
         showLoginModal: false,
-        invite_info: false
+        invite_info: true
     },
     onLoad: function(options) {
         var _this = this;
