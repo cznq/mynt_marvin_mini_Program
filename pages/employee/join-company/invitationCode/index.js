dced5aa73c821c53b477bd1b57821d924b1ada20
suten <sutenwang@slightech.com>
2019-01-18 16:28:45 +0800

    fix(invitation code): 加入公司输入邀请码增加确定button
    http://jira.mynt.com/browse/MARVINAG-919

--- pages/employee/join-company/invitationCode/index.js
+++ pages/employee/join-company/invitationCode/index.js
@@ -5,69 +5,81 @@ Page({
         mainTitle: '输入邀请码',
         hint: '二维码与邀请码来自于企业内部人员的分享,可\n向企业员工或管理员索要',
         codevalue: '',
-        isfocus: true
+        isfocus: true,
+        position: 'fixed',
+        disabled: true
     },
-    companyCode: function(e) {
+    bindfocus(e) {
+        let _this = this;
+        _this.setData({
+            position: 'static',
+            padding: '20rpx'
+        })
+    },
+    bindblur(e) {
+        let _this = this;
+        _this.setData({
+            position: 'fixed',
+            padding: 0
+        })
+    },
+    confirmCode(e) {
+        var company_code = e.detail.value.code;
         var _this = this;
-        var company_code = e.detail.value.replace(/\s/ig, '');
-        //判断公司码不能为汉字
-        if (app.Util.checkCode(company_code) == true) {
-            _this.setData({
-                isfocus: false
-            })
-            toast.showToast(this, {
-                toastStyle: 'toast',
-                title: '你输入的码有误,请重新输入',
-                duration: 2000,
-                mask: false,
-                cb: function() {
-                    _this.setData({
-                        codevalue: '',
-                        isfocus: true
+        app.Util.network.POST({
+            url: app.globalData.BASE_API_URL,
+            params: {
+                service: 'company',
+                method: 'get_company_info',
+                data: JSON.stringify({
+                    company_code: company_code,
+                })
+            },
+            success: res => {
+                if (res.data.sub_code == 0) {
+                    wx.navigateTo({
+                        url: '../confirmCompanyInformation/index?company_code=' + company_code,
                     })
+                } else {
+                    _this.setData({
+                        isfocus: false
+                    })
+                    toast.showToast(_this, {
+                        toastStyle: 'toast',
+                        title: '你输入的码有误,请重新输入',
+                        duration: 2000,
+                        mask: false,
+                        cb: function() {
+                            _this.setData({
+                                isfocus: true
+                            })
+                        }
+                    });
                 }
-            });
+            },
+            fail: res => {
+                console.log('fail');
+            }
+        })
+    },
+    companyCode(e) {
+        var _this = this;
+        var reg = /[\u4e00-\u9fa5]/g;
+        var codevalue = e.detail.value.replace(/\s/ig, '');
+        if (app.Util.checkCode(codevalue) == true) {
+            var codevalue = e.detail.value.replace(reg, "");
+            _this.setData({
+                codevalue: codevalue
+            })
         }
         //判断公司码8位输入完请求
-        if (company_code.length == 8) {
-            var company_code = company_code;
-            var union_id = wx.getStorageSync('xy_session');
-            app.Util.network.POST({
-                url: app.globalData.BASE_API_URL,
-                params: {
-                    service: 'company',
-                    method: 'get_company_info',
-                    data: JSON.stringify({
-                        company_code: company_code,
-                        union_id: union_id
-                    })
-                },
-                success: res => {
-                    if (res.data.sub_code == 0) {
-                        wx.navigateTo({
-                            url: '../confirmCompanyInformation/index?company_code=' + company_code,
-                        })
-                    } else {
-                        _this.setData({
-                            isfocus: false
-                        })
-                        toast.showToast(_this, {
-                            toastStyle: 'toast',
-                            title: '你输入的码有误,请重新输入',
-                            duration: 2000,
-                            mask: false,
-                            cb: function() {
-                                _this.setData({
-                                    codevalue: '',
-                                    isfocus: true
-                                })
-                            }
-                        });
-                    }
-                },
-                fail: res => {
-                    console.log('fail');
-                }
+        if (codevalue.length == 8) {
+            _this.setData({
+                disabled: false
+            })
+        } else {
+            _this.setData({
+                disabled: true
             })
         }
     }
