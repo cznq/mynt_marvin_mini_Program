f8ca03755a542eadb8a1de3a84bf898b710a4adb
raiyen <raiyen@slightech.com>
2019-01-21 09:55:47 +0800

    fix(boss): 高管列式页面修改，增加删除样式

--- pages/employee/senior-executive/bossList/index.js
+++ pages/employee/senior-executive/bossList/index.js
@@ -1,4 +1,5 @@
 // pages/employee/senior-executive/bossList/index.js
+var toast = require('../../../../templates/showToast/showToast');
 const app = getApp();
 Page({
 
@@ -12,7 +13,8 @@ Page({
     leaderList:null,
     limit_count:'',
     setNum:'',
-    btnShow:false,
+    isNull:true,
+    showRemove:false
   },
 
   /**
@@ -23,6 +25,7 @@ Page({
       limit_count:options.limit_count
     })   
     this.getBossList();
+    console.log(this.data.isNull)
   },
   /**
    * 获取员工列表数据
@@ -41,23 +44,44 @@ Page({
       success: res => {
         console.log(res.data.result);
         if(res.data.result){  
-          let n = that.data.limit_count - res.data.result.leader.length;
           that.setData({
             leaderList:res.data.result.leader,
-            setNum:n
           })   
-          if (that.data.setNum <= 0) {
+          if (that.data.leaderList.length == 3) {
             that.setData({             
-              setNum:0,
-              btnShow: true
+              isNull:false,
             })
-          } 
+          }else{
+            that.setData({             
+              isNull:true,
+            })
+          }
         }
       }
     })
   },
   onShow: function () {
   },
+  //管理高管
+  manage:function(){
+    this.setData({
+      showRemove:!this.data.showRemove
+    }) 
+  },
+  remove:function(e){
+    let union_id = e.currentTarget.dataset.id;
+    toast.showToast(this, {
+      toastStyle: 'toast2',
+      title: '移除该高管',
+      introduce: '是否确认移除该高管的权限？',
+      mask: true,
+      isSure: true,
+      sureText: '确定',
+      isClose: true,
+      closeText: '取消'
+    });
+  },
+  //去选择员工
   goSelect:function(e){
     wx.navigateTo({
       url: '/pages/employee/staff-choose-list/index?from='+e.currentTarget.dataset.from,
