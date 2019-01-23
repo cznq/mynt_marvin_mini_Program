3b8e85f505540ef3720addecddf3fd61f4a108db
suten <sutenwang@slightech.com>
2019-01-23 18:15:02 +0800

    feat(visitor list): 访客列表页面逻辑

--- pages/visitor-list/list/index.js
+++ pages/visitor-list/list/index.js
@@ -1,4 +1,7 @@
 // pages/visitor-list/list/index.js
+var app = getApp()
+var toast = require('../../../templates/showToast/showToast');
+
 Page({
     /**
      * 页面的初始数据
@@ -11,7 +14,12 @@ Page({
         visitor_type: 0,
         time_range: 0,
         page: 1,
+        total_page: 1,
         list: {},
+        sData: {},
+        searchModal: false,
+        searchFocus: false,
+        keyWord: '',
         noneData: {
             textInfo: '暂无数据',
             buttonText: '邀请访客',
@@ -26,11 +34,15 @@ Page({
             _this.setData({
                 timeChoose: false,
                 typeChoose: false,
+                searchModal: false,
+                searchFocus: false
             })
         } else {
             _this.setData({
                 timeChoose: true,
                 typeChoose: false,
+                searchModal: false,
+                searchFocus: false
             })
         }
     },
@@ -62,7 +74,11 @@ Page({
         _this.setData({
             seleTime: seleTime,
             timeText: timeText,
-            time_range: time_range
+            time_range: time_range,
+            searchModal: false,
+            searchFocus: false,
+            page: 1,
+            list: {}
         }, () => {
             _this.showList();
         });
@@ -73,12 +89,16 @@ Page({
         if (typeChoose) {
             _this.setData({
                 typeChoose: false,
-                timeChoose: false
+                timeChoose: false,
+                searchModal: false,
+                searchFocus: false,
             })
         } else {
             _this.setData({
                 typeChoose: true,
-                timeChoose: false
+                timeChoose: false,
+                searchModal: false,
+                searchFocus: false
             })
         }
     },
@@ -106,40 +126,118 @@ Page({
         _this.setData({
             seleType: seleType,
             typeText: typeText,
-            visitor_type: visitor_type
+            visitor_type: visitor_type,
+            searchModal: false,
+            searchFocus: false,
+            page: 1,
+            list: {}
         }, () => {
             _this.showList();
         });
     },
-    // 调取接口查询访客列表
-    showList: function() {
-        console.log(123);
-    },
     // 搜索
-    startSearchInput: function() {
+    showSearch: function() {
         let _this = this;
         _this.setData({
-            searchModal: true
+            searchModal: true,
+            searchFocus: true,
+            timeChoose: false,
+            typeChoose: false,
         });
     },
     searchCancel: function() {
         let _this = this;
         _this.setData({
-            searchModal: false
+            searchModal: false,
+            searchFocus: false,
+            keyWord: ''
         });
     },
-    // 空页面跳转到邀请页
-    emptyButton: function() {
-        wx.navigateTo({
-            url: '/pages/invite-visitor/edit/index',
+    searchInput: function(e) {
+        let keyWord = e.detail.value;
+        let _this = this;
+        _this.setData({
+            keyWord: keyWord
+        });
+    },
+    searchSubmit: function() {
+        let _this = this;
+        _this.showList();
+    },
+    // 调取接口查询访客列表
+    showList: function() {
+        let _this = this;
+        app.Util.network.POST({
+            url: app.globalData.BASE_API_URL,
+            params: {
+                service: 'company',
+                method: 'get_info',
+                data: JSON.stringify({
+
+                })
+            },
+            success: res => {
+                console.log("获取公司信息:", res.data);
+                if (res.data.sub_code == 0) {
+                    var company_id = res.data.result.company_id;
+                    app.Util.network.POST({
+                        url: app.globalData.BASE_API_URL,
+                        params: {
+                            service: 'visitor',
+                            method: 'get_visitor_list',
+                            data: JSON.stringify({
+                                company_id: company_id,
+                                time_range: _this.data.time_range,
+                                visitor_type: _this.data.visitor_type,
+                                page: _this.data.page,
+                                visitor_name: _this.data.keyWord,
+                            })
+                        },
+                        success: res => {
+                            console.log("获取访客信息:", res.data.result);
+                            if (res.data.sub_code == 0 && res.data.result) {
+                                let rtData = res.data.result;
+                                if (rtData.count == 0) {
+                                    _this.setData({
+                                        'noneData.show': true,
+                                    });
+                                } else {
+                                    // 如果搜索条件不为空则放入搜素对象内,否则放入正常数据对象
+                                    if (_this.data.keyWord.length != 0) {
+                                        _this.setData({
+                                            totalPage: rtData.total_page,
+                                            sData: rtData.data
+                                        })
+                                    } else {
+                                        _this.setData({
+                                            totalPage: rtData.total_page,
+                                            list: rtData.data
+                                        })
+                                    }
+                                }
+                            } else {
+                                _this.setData({
+                                    'noneData.show': true,
+                                });
+                            }
+                        },
+                        fail: res => {
+                            console.log(res);
+                        }
+                    })
+                }
+            },
+            fail: res => {
+                console.log(res);
+            }
         })
     },
-
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
         let _this = this;
+        _this.showList();
     },
 
     /**
@@ -172,12 +270,69 @@ Page({
      */
     onReachBottom: function() {
         let _this = this;
-        _this.setData({
-            page: _this.data.page + 1,
-        }, () => {
-            _this.showList();
-        });
-        console.log(_this.data);
+        if (_this.data.total_page == _this.data.page) {
+            return false;
+        }
+        app.Util.network.POST({
+            url: app.globalData.BASE_API_URL,
+            params: {
+                service: 'company',
+                method: 'get_info',
+                data: JSON.stringify({
+
+                })
+            },
+            success: res => {
+                console.log("获取公司信息:", res.data);
+                if (res.data.sub_code == 0) {
+                    var company_id = res.data.result.company_id;
+                    app.Util.network.POST({
+                        url: app.globalData.BASE_API_URL,
+                        params: {
+                            service: 'visitor',
+                            method: 'get_visitor_list',
+                            data: JSON.stringify({
+                                company_id: company_id,
+                                time_range: _this.data.time_range,
+                                visitor_type: _this.data.visitor_type,
+                                page: _this.data.page,
+                                visitor_name: _this.data.keyWord,
+                            })
+                        },
+                        success: res => {
+                            console.log("获取访客信息:", res.data.result);
+                            if (res.data.sub_code == 0 && res.data.result) {
+                                let rtData = res.data.result;
+                                // 如果搜索条件不为空则放入搜素对象内,否则放入正常数据对象
+                                if (_this.data.keyWord.length != 0) {
+                                    _this.setData({
+                                        totalPage: rtData.total_page,
+                                        page: _this.data.page + 1,
+                                        sData: _this.data.sData.concat(rtData.data)
+                                    })
+                                } else {
+                                    _this.setData({
+                                        totalPage: rtData.total_page,
+                                        page: _this.data.page + 1,
+                                        list: _this.data.list.concat(rtData.data)
+                                    })
+                                }
+                            } else {
+                                _this.setData({
+                                    'noneData.show': true,
+                                });
+                            }
+                        },
+                        fail: res => {
+                            console.log(res);
+                        }
+                    })
+                }
+            },
+            fail: res => {
+                console.log(res);
+            }
+        })
     },
 
     /**
