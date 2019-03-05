7c6473c55e974e4ada9f0adf01a97197654fb4fe
fantingsheng <fants0230@sina.com>
2019-03-05 17:43:55 +0800

    feat(nonedata): 修改无数据页面模版
    n

---
+++ assets/pages/staff/list/index.js
@@ -0,0 +1,78 @@
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    staffCount: 0,
+    staffList: '',
+    noneData: {
+      pointer: true,
+      title: '暂无资产',
+      textInfo: '添加人员，帮助您添加资产、核销缴费',
+      picSize: {
+        width: '180rpx',
+        height: '156rpx',
+        marginTop: '178rpx'
+      },
+      pic: 'https://slightech-marvin-wechat.oss-cn-hangzhou.aliyuncs.com/marvin-mini-program/assets/no-staff%402x.png'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
