b78b0770f44075b69a78d846e26bdb427d2e20b9
suten <sutenwang@slightech.com>
2019-03-12 17:55:01 +0800

    feat(edit asset): 编辑资产显示页跳转

--- assets/pages/management/building_room/index.js
+++ assets/pages/management/building_room/index.js
@@ -2,124 +2,126 @@
 var app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    building_id:'',
-    owner_id: '',
-    employee_id: '',
-    building_name:'',
-    building_address:'',
-    cd:''
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        building_id: '',
+        owner_id: '',
+        employee_id: '',
+        building_name: '',
+        building_address: '',
+        cd: ''
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that= this;
-    that.setData({ 
-      building_id:JSON.parse(options.buildingInfo).building_id,
-      owner_id: JSON.parse(options.buildingInfo).owner_id,
-      employee_id: JSON.parse(options.buildingInfo).employee_id,
-      building_name:JSON.parse(options.buildingInfo).building_name,
-      building_address:JSON.parse(options.buildingInfo).building_address
-    })
-    wx.setNavigationBarTitle({
-      title: that.data.building_name
-    })
-    app.Util.networkUrl.postUrl({
-      url: app.globalData.BASE_ASSET_URL + '/asset/building/list',
-      params: {
-        data: JSON.stringify({
-          owner_id:that.data.owner_id,
-          building_id: that.data.building_id,
-          employee_id:that.data.employee_id
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        var that = this;
+        that.setData({
+            building_id: JSON.parse(options.buildingInfo).building_id,
+            owner_id: JSON.parse(options.buildingInfo).owner_id,
+            employee_id: JSON.parse(options.buildingInfo).employee_id,
+            building_name: JSON.parse(options.buildingInfo).building_name,
+            building_address: JSON.parse(options.buildingInfo).building_address
         })
-      },
-      success: res => {
-        if (res.data.result) {
-          that.setData({
-            cd:res.data.result.asset_list
-          })
-        }
-      }
-    })
-  },
-  addRoom:function(e){
-    var buildingAsset = JSON.stringify({
-      buildingInfo:{
-        id: this.data.building_id,
-        name: this.data.building_name,
-        address: this.data.building_address,
-      },
-      owner_id: this.data.owner_id,
-      employee_id: this.data.employee_id
-    })
-    wx.navigateTo({
-      url: '../add-asset/index?buildingAsset=' + buildingAsset+'&from='+e.currentTarget.dataset.from,
-    })
-  },
-  assetDetail:function(e){
-    var params = JSON.stringify({
-      building_address:this.data.building_address,
-      building_name:this.data.building_name,
-      id:e.currentTarget.dataset.id,//资产id
-      area:e.currentTarget.dataset.area,
-      room_number:e.currentTarget.dataset.room,//房间号
-      floor_index:e.currentTarget.dataset.floor,//楼层索引
-    })
-    wx.navigateTo({
-      url: '../edit-asset/index?params=' + params,
-    })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        wx.setNavigationBarTitle({
+            title: that.data.building_name
+        })
+        app.Util.networkUrl.postUrl({
+            url: app.globalData.BASE_ASSET_URL + '/asset/building/list',
+            params: {
+                data: JSON.stringify({
+                    owner_id: that.data.owner_id,
+                    building_id: that.data.building_id,
+                    employee_id: that.data.employee_id
+                })
+            },
+            success: res => {
+                if (res.data.result) {
+                    that.setData({
+                        cd: res.data.result.asset_list
+                    })
+                }
+            }
+        })
+    },
+    addRoom: function(e) {
+        var buildingAsset = JSON.stringify({
+            buildingInfo: {
+                id: this.data.building_id,
+                name: this.data.building_name,
+                address: this.data.building_address,
+            },
+            owner_id: this.data.owner_id,
+            employee_id: this.data.employee_id
+        })
+        wx.navigateTo({
+            url: '../add-asset/index?buildingAsset=' + buildingAsset + '&from=' + e.currentTarget.dataset.from,
+        })
+    },
+    assetDetail: function(e) {
+        var params = JSON.stringify({
+            building_address: this.data.building_address,
+            owner_id: this.data.owner_id,
+            employee_id: this.data.employee_id,
+            building_name: this.data.building_name,
+            id: e.currentTarget.dataset.id, //资产id
+            area: e.currentTarget.dataset.area,
+            room_number: e.currentTarget.dataset.room, //房间号
+            floor_index: e.currentTarget.dataset.floor, //楼层索引
+        })
+        wx.navigateTo({
+            url: '../edit-asset/index?params=' + params,
+        })
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  }
+    }
 })
--- assets/pages/management/edit-asset/index.js
+++ assets/pages/management/edit-asset/index.js
@@ -7,28 +7,101 @@ Page({
      * 页面的初始数据
      */
     data: {
-        room: "望京大厦E栋 16层1603",
-        area: "建筑面积：359㎡",
-        address: "杭州市萧山区景贤路21号"
+        building_address: "",
+        building_name: "",
+        id: "",
+        area: "",
+        room_number: "",
+        floor_index: "",
+        addType: "roomList",
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
+        var _this = this;
+        _this.setData({
+            building_address: JSON.parse(options.building_address),
+            building_name: JSON.parse(options.building_name),
+            employee_id: JSON.parse(options.employee_id),
+            id: JSON.parse(options.id), //资产id
+            area: JSON.parse(options.area),
+            room_number: JSON.parse(options.room), //房间号
+            floor_index: JSON.parse(options.floor), //楼层索引
+        })
         wx.setNavigationBarTitle({
-            title: "16330"
+            title: JSON.parse(options.room_number)
         });
-        // this.setData({
-        //     owner_id: JSON.parse(options.params).owner_id,
-        //     employee_id: JSON.parse(options.params).employee_id,
-        //     nextStep: true
-        // })
+    },
+
+    jumpUrl: function() {
+        var _this = this;
+        var buildingAsset = JSON.stringify({
+            buildingInfo: {
+                id: _this.data.building_id,
+                name: _this.data.building_name,
+                address: _this.data.building_address,
+            },
+            owner_id: _this.data.owner_id,
+            employee_id: _this.data.employee_id
+        })
+        wx.navigateTo({
+            url: '../add-asset/index?buildingAsset=' + buildingAsset + '&from=' + _this.data.addType,
+        })
     },
 
     removeAsset: function() {
+        toast.showToast(this, {
+            toastStyle: 'toast4',
+            title: '确认删除该资产?',
+            introduce: '删除后，该资产将与您解除绑定关系，并且在资产列表中消失。',
+            mask: true,
+            isSure: true,
+            sureText: '确认删除',
+            isClose: true,
+            closeText: '再考虑一下'
+        });
+    },
+    bindToastSure: function(value) {
         var _this = this;
+        app.Util.networkUrl.postUrl({
+            url: app.globalData.BASE_ASSET_URL + "/asset/delete",
+            params: {
+                data: JSON.stringify({
+                    union_id: wx.getStorageSync('xy_session'),
+                    owner_id: _this.data.owner_id,
+                    id: _this.data.id
+                })
+            },
+            success: res => {
+                console.log('asset/delete return:');
+                console.log(res);
+                if (res.data.sub_code == "SUCCESS") {
+                    wx.navigateBack({});
+                } else {
+                    _this.setData({
+                        isfocus: false
+                    })
+                    toast.showToast(this, {
+                        toastStyle: 'toast',
+                        title: res.data.sub_msg,
+                        duration: 2000,
+                        mask: false,
+                    });
+                }
+            },
+            fail: res => {
+                console.log('fail');
+            }
+        })
+        toast.hideToast();
+    },
 
+    bindToastClose: function() {
+        var _this = this;
+        console.log("取消删除");
+        toast.hideToast();
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
