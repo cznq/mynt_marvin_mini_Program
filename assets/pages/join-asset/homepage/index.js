26a81bc8796faaeddd26648e0588f414571bc129
Wzj <jansonkwan@slightech.com>
2019-03-12 18:28:14 +0800

    fix bug

--- assets/pages/join-asset/homepage/index.js
+++ assets/pages/join-asset/homepage/index.js
@@ -2,106 +2,106 @@
 var app = getApp();
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        headlinesT1: "欢迎使用",
-        headlinesT2: "小觅资产管理系统",
-        explainText: "资产管理便捷高效，多套房源一手掌握， 线上收租方便快捷。",
-        buttonText: "立即体验"
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    headlinesT1: "欢迎使用",
+    headlinesT2: "小觅资产管理系统",
+    explainText: "资产管理便捷高效，多套房源一手掌握， 线上收租方便快捷。",
+    buttonText: "立即体验"
+  },
 
-    jumpUrl: function() {
-        var _this = this;
-        app.Util.networkUrl.postUrl({
-            url: app.globalData.BASE_ASSET_URL + "/employee/get/union_id",
-            params: {
-                data: JSON.stringify({
-                    union_id: wx.getStorageSync('xy_session'),
-                })
-            },
-            success: res => {
-                console.log('owner-get-verifycode return:');
-                console.log(res);
-                if (res.data.sub_code == "SUCCESS") {
-                    let rinfo = res.data.result;
-                    var params = JSON.stringify({
-                        building_address: rinfo.building_address,
-                        owner_id: rinfo.owner_id,
-                        phone: rinfo.phone,
-                        role: rinfo.role,
-                        asset_list: rinfo.asset_list,
-                        privilege_list: rinfo.privilege_list
-                    })
-                    wx.navigateTo({
-                        url: '../asset-;ost/index?params=' + params,
-                    })
-                } else {
-                    wx.navigateTo({
-                        url: "../invite-code/index",
-                    });
-                }
-            },
-            fail: res => {
-                console.log('fail');
-            }
+  jumpUrl: function() {
+    var _this = this;
+    app.Util.networkUrl.postUrl({
+      url: app.globalData.BASE_ASSET_URL + "/employee/get/union_id",
+      params: {
+        data: JSON.stringify({
+          union_id: wx.getStorageSync('xy_session'),
         })
-    },
+      },
+      success: res => {
+        console.log('owner-get-verifycode return:');
+        console.log(res);
+        if (res.data.sub_code == "SUCCESS") {
+          let rinfo = res.data.result;
+          var params = JSON.stringify({
+            building_address: rinfo.building_address,
+            owner_id: rinfo.owner_id,
+            phone: rinfo.phone,
+            role: rinfo.role,
+            asset_list: rinfo.asset_list,
+            privilege_list: rinfo.privilege_list
+          })
+          wx.navigateTo({
+            url: '../../management/assets-list/index?params=' + params,
+          })
+        } else {
+          wx.navigateTo({
+            url: "../invite-code/index",
+          });
+        }
+      },
+      fail: res => {
+        console.log('fail');
+      }
+    })
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    },
+  },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
-    }
+  }
 })
--- assets/pages/management/assets-list/index.js
+++ assets/pages/management/assets-list/index.js
@@ -7,20 +7,20 @@ Page({
    */
   data: {
     isIphoneX: app.globalData.isIphoneX,
-    owner_id:'',//房东id
-    employee_id:'',
-    assetName:'',
-    hasAssets:true,
-    asset_list:'',
+    owner_id: '', //房东id
+    employee_id: '',
+    assetName: '',
+    hasAssets: true,
+    asset_list: '',
     noneAssets: {
-      pointer: true,      
+      pointer: true,
       title: '暂无资产',
       textInfo: '创建资产，开启智能收费核销',
-      pic:app.globalData.BASE_IMG_URl +'assets/empty_no_asset%402x.png',
-      picSize:{
-        width:180+'rpx',
-        height:144+'rpx',
-        marginTop: 306+'rpx'
+      pic: app.globalData.BASE_IMG_URl + 'assets/empty_no_asset%402x.png',
+      picSize: {
+        width: 180 + 'rpx',
+        height: 144 + 'rpx',
+        marginTop: 306 + 'rpx'
       }
     },
   },
@@ -28,46 +28,47 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var that= this;
-    that.setData({ 
-      owner_id:JSON.parse(options.params).owner_id,
-      employee_id:JSON.parse(options.params).employee_id,
-      assetName:JSON.parse(options.params).assetName
+  onLoad: function(options) {
+    console.log('options:', options);
+    var that = this;
+    that.setData({
+      owner_id: JSON.parse(options.params).owner_id,
+      employee_id: JSON.parse(options.params).employee_id,
+      assetName: JSON.parse(options.params).assetName
     })
     app.Util.networkUrl.postUrl({
-      url: app.globalData.BASE_ASSET_URL+'/asset/building_list',
+      url: app.globalData.BASE_ASSET_URL + '/asset/building_list',
       params: {
         data: JSON.stringify({
           owner_id: this.data.owner_id,
-          employee_id:this.data.employee_id//员工身份请求时，传此值
+          employee_id: this.data.employee_id //员工身份请求时，传此值
         })
       },
       success: res => {
         if (res.data.result) {
           that.setData({
-            asset_list:res.data.result.asset_count_list,
-            hasAssets:true
+            asset_list: res.data.result.asset_count_list,
+            hasAssets: true
           })
-        }else{
+        } else {
           that.setData({
-            hasAssets:false
+            hasAssets: false
           })
         }
       }
     })
   },
-  addAsset:function(e){
+  addAsset: function(e) {
     var params = JSON.stringify({
       owner_id: this.data.owner_id,
-      employee_id:this.data.employee_id
+      employee_id: this.data.employee_id
     })
     wx.navigateTo({
-      url: '../add-asset/index?params='+ params+'&from='+e.currentTarget.dataset.from,
+      url: '../add-asset/index?params=' + params + '&from=' + e.currentTarget.dataset.from,
     })
   },
   //获取楼宇下资产列表信息
-  getBuildingAsset:function(e){
+  getBuildingAsset: function(e) {
     var buildingInfo = JSON.stringify({
       building_id: e.currentTarget.dataset.id,
       building_name: e.currentTarget.dataset.building_name,
@@ -76,55 +77,55 @@ Page({
       employee_id: this.data.employee_id
     })
     wx.navigateTo({
-      url: '../building_room/index?buildingInfo='+ buildingInfo,
+      url: '../building_room/index?buildingInfo=' + buildingInfo,
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
