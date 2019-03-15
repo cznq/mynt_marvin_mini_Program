4979c71623afd886404e6a96bed0ffe4ff13cd5d
fantingsheng <fants0230@sina.com>
2019-03-15 16:49:26 +0800

    feat(vip): 套餐支付页面列表渲染

--- pages/businessService/servicePay/index.js
+++ pages/businessService/servicePay/index.js
@@ -1,20 +1,37 @@
 // pages/businessService/servicePay/index.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    isIphoneX: app.globalData.isIphoneX,
     pck: [{
-      price: 2045,
-      time: 12,
-      discount: 7
+      price: [7, 8, 4, '.', 5],
+      timeLength: 12,
+      discount: 7,
+      savedValue: 490,
+      pckInfo: '原价￥954，限时折扣'
     },{
-      price: 2045,
-      time: 12,
-      discount: 7
-    }],
-    price: [7, 8, 4, '.', 5],
+      price: [2, 7, 3],
+      timeLength: 6,
+      discount: 7,
+      savedValue: 250,
+        pckInfo: '原价￥784，限时折扣'
+      },{
+        price: [6, 1, 9],
+        timeLength: 3,
+        discount: 4,
+        savedValue: 120,
+        pckInfo: '原价￥365，限时折扣'
+      },{
+        price: [8, 4, 9],
+        timeLength: 1,
+        discount: null,
+        savedValue: null,
+        pckInfo: '原价￥12，限时折扣'
+      }],
     chooseId: 0
   },
 
@@ -22,13 +39,49 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCompany(this);
   },
 
   choosePck: function(e) {
     console.log(e);
+    let chid = e.currentTarget.dataset.pckid;
+    this.setData({
+      chooseId: chid
+    })
   },
 
+  packagePay() {
+    
+    wx.showToast({
+      title: '你选择的套餐是' + this.data.pck[this.data.chooseId].timeLength + '个月',
+      icon: 'none'
+    })
+  },
+
+  /**
+   * 获取公司信息
+   */
+  getCompany: function (_this) {
+    app.Util.network.POST({
+      url: app.globalData.BASE_API_URL,
+      params: {
+        service: 'company',
+        method: 'get_info',
+        data: JSON.stringify({
+          union_id: wx.getStorageSync('xy_session')
+        })
+      },
+      success: res => {
+        if (res.data.result) {
+          _this.setData({
+            cmpInfo: res.data.result
+          })
+          
+        }
+        
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
