971e8db56534e767b008043efce9893c137050f1
Wzj <jansonkwan@slightech.com>
2019-03-22 16:48:53 +0800

    feat

--- pages/e-card/detail/index.js
+++ pages/e-card/detail/index.js
@@ -3,7 +3,46 @@ import {
 } from './pca.js';
 const app = getApp();
 var count = 1;
+import initCalendar, {
+  jump,
+  getSelectedDay
+} from '../../../components/calendar/main.js';
 
+const conf = {
+  multi: true, // 是否开启多选,
+  disablePastDay: true, // 是否禁选过去的日期
+  /**
+   * 初始化日历时指定默认选中日期，如：'2018-3-6' 或 '2018-03-06'
+   * 注意：若想初始化时不默认选中当天，则将该值配置为除 undefined 以外的其他非值即可，如：空字符串, 0 ,false等。
+   */
+  // defaultDay: '', // 初始化后是否默认选中指定日期
+  noDefault: true, // 初始化后是否自动选中当天日期，优先级高于defaultDay配置，两者请勿一起配置
+  /**
+   * 选择日期后执行的事件
+   * @param { object } currentSelect 当前点击的日期
+   * @param { array } allSelectedDays 选择的所有日期（当mulit为true时，才有allSelectedDays参数）
+   */
+  afterTapDay: (currentSelect, allSelectedDays) => {
+    console.log('currentSelect:', currentSelect, 'allSelectedDays:', allSelectedDays);
+  },
+  /**
+   * 当改变月份时触发
+   * @param { object } current 当前年月
+   * @param { object } next 切换后的年月
+   */
+  // whenChangeMonth: (current, next) => {},
+  /**
+   * 日期点击事件（此事件会完全接管点击事件）
+   * @param { object } currentSelect 当前点击的日期
+   * @param { object } event 日期点击事件对象
+   */
+  // onTapDay(currentSelect, event) {},
+  /**
+   * 日历初次渲染完成后触发事件，如设置事件标记
+   * @param { object } ctx 当前页面实例
+   */
+  afterCalendarRender(ctx) {},
+}
 Page({
 
   /**
@@ -41,6 +80,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    initCalendar(conf);
+    // jump({
+    //   year: '2019',
+    //   month: '3',
+    //   day: '25'
+    // })
+    // console.log('getSelectedDay', getSelectedDay());
     var that = this;
 
     if (options.invitation_id) {
