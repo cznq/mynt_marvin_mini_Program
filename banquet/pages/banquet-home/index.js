b1267cbb0cb44fa842f28d30e6a745555d4bb3f7
raiyen <raiyen@slightech.com>
2019-04-10 18:03:43 +0800

    feat(banquet): 1.商务宴请首页 详情页（小米协议页面副本暂未修改详细2.预定酒店页面及页面功能（日历、时间段、增加房间及入住人）

--- banquet/pages/banquet-home/index.js
+++ banquet/pages/banquet-home/index.js
@@ -1,23 +1,211 @@
 // banquet/pages/banquet-home/index.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    slide_data: {
+      slide_img: {},
+      indicatorDots: false,
+      autoplay: true,
+      interval: 5000,
+      duration: 500
+    },
+    shopList: null,
+    tabSelected: 'hotel',
+    selectedType: 2,
+    tabList: [{
+      typeid: 2,
+      id: 'hotel',
+      title: '酒店'
+    },
+    {
+      typeid: 0,
+      id: 'business',
+      title: '宴请'
+    },
+    ],
+    tabFixed: false,
+    is_vip: false,
+    employeeInfo: null,
+    taboffsetTop: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options);
+    if (options.tabSelected && options.selectedType) {
+      this.setData({ tabSelected: options.tabSelected, selectedType: options.selectedType });
+    }
+    var self = this;
 
+    self.getCommerceList(self.data.selectedType);
+    self.getEmployeeInfo();
+
+    self.getOffsetTop();
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取tab 距离上面的距离
    */
+  getOffsetTop() {
+    var that = this;
+    const query = wx.createSelectorQuery()
+    query.select('#selTab').boundingClientRect()
+    query.selectViewport().scrollOffset()
+    query.exec(function (res) {
+      console.log(res[0].top);
+      that.setData({
+        taboffsetTop: res[0].top
+      })
+    })
+  },
+
+  /**
+   * 获取员工信息
+   */
+  getEmployeeInfo() {
+    var that = this;
+    app.Util.network.POST({
+      url: app.globalData.BASE_API_URL,
+      params: {
+        service: 'company',
+        method: 'get_company_service_status',
+        data: JSON.stringify({
+          union_id: wx.getStorageSync('xy_session'),
+          service_key: 'EMPLOYEE_BENIFIT'
+        })
+      },
+      success: res => {
+        if (res.data.result) {
+          if (res.data.result.service_status !== 0) {
+            console.log('kaitong');
+            app.Util.network.POST({
+              url: app.globalData.BENIFIT_API_URL,
+              params: {
+                service: 'company',
+                method: 'get_employee_info',
+                union_id: wx.getStorageSync('xy_session'),
+                data: JSON.stringify({})
+              },
+              success: res => {
+                console.log(res);
+                if (res.data.result) {
+                  that.setData({
+                    is_vip: true
+                  })
+                } else {
+                  that.setData({
+                    is_vip: false
+                  })
+                }
+              }
+            })
+          } else {
+            that.setData({
+              is_vip: false
+            })
+          }
+        } else {
+          that.setData({
+            is_vip: false
+          })
+        }
+      }
+    })
+  },
+
+
+  /**
+   * 监听滚动，tab置顶
+   */
+  onPageScroll: function (e) {
+    console.log(e.scrollTop);
+    var that = this;
+
+    if (e.scrollTop < that.data.taboffsetTop) {
+      that.setData({
+        tabFixed: false
+      });
+    } else {
+      that.setData({
+        tabFixed: true
+      });
+    }
+  },
+
+  /**
+   * 切换Tab
+   */
+  changeTab: function (e) {
+    var typedId = e.currentTarget.dataset.typeid;
+    var selectedId = e.currentTarget.dataset.selectid;
+    this.setData({ tabSelected: selectedId, selectedType: typedId });
+    this.getCommerceList(typedId);
+  },
+
+
+  /**
+   * 获取首页的商家列表
+   */
+  getCommerceList(commerceType) {
+    var that = this;
+    var slide_img = "slide_data.slide_img";
+    app.Util.network.POST({
+      url: app.globalData.BENIFIT_API_URL,
+      params: {
+        service: 'commerce',
+        method: 'get_commerce_list',
+        union_id: wx.getStorageSync('xy_session'),
+        data: JSON.stringify({
+          type: commerceType
+        })
+      },
+      success: res => {
+        if (res.data.sub_code == 0 && res.data.result) {
+          if (commerceType == 2) {
+            that.setData({
+              shopList: that.transData(res.data.result),
+              [slide_img]: res.data.result.hompage
+            })
+          } else {
+            that.setData({
+              shopList: res.data.result,
+              [slide_img]: res.data.result.hompage
+            })
+          }
+        } else {
+          that.setData({
+            shopList: null
+          })
+        }
+      }
+    })
+  },
+
+  transData(preData) {
+    for (var i = 0; i < preData.length; i++) {
+      preData[i].agreement_price = String(preData[i].agreement_price).split('');
+    }
+    return preData;
+  },
+
+  /**
+   * 跳转到商家详情
+   */
+  redirectCommerce: function (e) {
+    var commerce_id = e.currentTarget.dataset.commerceid;
+    var commerce_type = e.currentTarget.dataset.commercetype;
+    wx.navigateTo({
+      url: '/banquet/pages/detail/index?commerce_id=' + commerce_id + '&commerce_type=' + commerce_type,
+    })
+  },
+
+
   onReady: function () {
 
   },
@@ -26,41 +214,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
---
+++ components/timeSlot/index.js
@@ -0,0 +1,80 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+   
+  },
+  /**
+ * 组件的初始数据
+ */
+  data: {
+    timeArr: [
+      { id: 0, time_slot: "00:00 — 01:00" },
+      { id: 2, time_slot: "02:00 — 03:00" },
+      { id: 3, time_slot: "03:00 — 04:00" },
+      { id: 4, time_slot: "04:00 — 05:00" },
+      { id: 5, time_slot: "05:00 — 06:00" },
+      { id: 6, time_slot: "06:00 — 07:00" },
+      { id: 7, time_slot: "07:00 — 08:00" },
+      { id: 8, time_slot: "08:00 — 09:00" },
+      { id: 9, time_slot: "09:00 — 10:00" },
+      { id: 10, time_slot: "10:00 — 11:00" },
+      { id: 11, time_slot: "11:00 — 12:00" },
+      { id: 12, time_slot: "12:00 — 13:00" },
+      { id: 13, time_slot: "13:00 — 14:00" },
+      { id: 14, time_slot: "14:00 — 15:00" },
+      { id: 15, time_slot: "15:00 — 16:00" },
+      { id: 16, time_slot: "16:00 — 17:00" },
+      { id: 17, time_slot: "17:00 — 18:00" },
+      { id: 18, time_slot: "18:00 — 19:00" },
+      { id: 19, time_slot: "19:00 — 20:00" },
+      { id: 20, time_slot: "20:00 — 21:00" },
+      { id: 21, time_slot: "21:00 — 22:00" },
+      { id: 22, time_slot: "22:00 — 23:00" },
+      { id: 23, time_slot: "23:00 — 00:00" },
+    ],
+    show: false,
+    value:[0],//默认当前显示在第几个时间段
+  },
+   /**
+   * 组件的方法列表
+   */
+  methods: {
+    //初始化选择器信息
+    toggleSelect: function () {
+      let show = this.data.show
+      if (show) return this.setData({ show: !show })
+      let hour = parseInt(new Date().getHours());    //返回小时数
+      this.setData({
+        show: !show,
+        value:[hour-1],//根据当前时间，默认选中最近的时间段
+      });
+    },
+    changeTimePicker: function (e) {
+      const val = e.detail.value
+      this.setData({
+        value: val,
+        time_slot: this.data.timeArr[val].time_slot
+      });
+    },
+    //关闭弹窗
+    // 取消
+    hideTime() {
+      this.setData({ show: false });
+    },
+    //确定
+    submit: function () {
+      let n = this.data.value
+      this.setData({
+        show: false,
+        time_slot: this.data.timeArr[n].time_slot
+      },
+      ()=>{       
+        const result=this.data.time_slot
+        this.triggerEvent('select', result)
+      }
+      );
+    }
+  }
+})
