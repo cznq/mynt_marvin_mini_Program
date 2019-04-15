5e9b06af1b9e047eaa72b53ebbfee4e0bf1b6c9a
Wzj <jansonkwan@slightech.com>
2019-04-15 11:25:42 +0800

    feat(add invoice):增加发票流程

--- banquet/pages/cashier/cashier.js
+++ banquet/pages/cashier/cashier.js
@@ -5,62 +5,95 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    invSwitch: false,
+    bankAccount: "", //银行账号
+    bankName: "", //银行名称
+    companyAddress: "", //单位地址
+    errMsg: "",
+    taxNumber: "", //抬头税号
+    telephone: "", //手机号码
+    title: "", //抬头名称
+    type: 0 //0 单位 1 个人
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+  },
+  chooseInvoece() {
+    let _that = this;
+    wx.chooseInvoiceTitle({
+      success(res) {
+        console.log('res:', res);
+        _that.setData({
+          title: res.title,
+          taxNumber: res.taxNumber,
+          bankAccount: res.bankAccount,
+          bankName: res.bankName,
+          type: res.type,
+          telephone: res.telephone,
+          companyAddress: res.companyAddress,
+          invSwitch: true
+        })
+
+      },
+      fail(res) {
+        wx.showToast({
+          title: res,
+          icon: 'none'
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- banquet/pages/hotel-reserve/index.js
+++ banquet/pages/hotel-reserve/index.js
@@ -8,15 +8,15 @@ Page({
   data: {
     commerce_id: null,
     commerce_type: null,
-    num:1,
-    max:'100',
-    selectData:'请选择到店时间',
+    num: 1,
+    max: '100',
+    selectData: '请选择到店时间',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     that.setData({
       commerce_id: options.commerce_id,
@@ -24,7 +24,7 @@ Page({
     })
     that.getDetailInfo(that.data.commerce_id);
   },
- /**
+  /**
    * 获取商家详情
    */
   getDetailInfo(commerce_id) {
@@ -50,12 +50,12 @@ Page({
       }
     })
   },
-   /* 点击减号 */
-   bindMinus: function() {
+  /* 点击减号 */
+  bindMinus: function() {
     var num = this.data.num;
     // 如果大于1时，才可以减
     if (num > 1) {
-      num --;
+      num--;
     }
     this.setData({
       num: num
@@ -64,7 +64,7 @@ Page({
   /* 点击加号 */
   bindPlus: function() {
     var num = this.data.num;
-    num ++;
+    num++;
     this.setData({
       num: num
     });
@@ -76,23 +76,23 @@ Page({
       num: num
     });
   },
-  remarksText:function (e) {
+  remarksText: function(e) {
     var value = e.detail.value;
     // 获取输入框内容的长度
     var len = parseInt(value.length);
     //最多字数限制
-    if(len > this.data.max) return;
+    if (len > this.data.max) return;
     this.setData({
-      currentWordNumber: len //当前字数  
+      currentWordNumber: len //当前字数
     })
   },
-  selectDate:function(){
+  selectDate: function() {
     wx.navigateTo({
       url: '../calendar-hotel/index'
     })
   },
   // 预计到店时间段
-  onSelect(e) { 
+  onSelect(e) {
     this.setData({
       selectData: e.detail
     })
@@ -100,22 +100,22 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     var that = this;
     let pages = getCurrentPages();
     let currPage = pages[pages.length - 1];
-    if (currPage.data.stayDays>0) {
+    if (currPage.data.stayDays > 0) {
       that.setData({
         checkInDate: currPage.data.checkInDate,
-        checkOutDate:currPage.data.checkOutDate,
-        stayDays:currPage.data.stayDays
+        checkOutDate: currPage.data.checkOutDate,
+        stayDays: currPage.data.stayDays
       })
     }
   },
