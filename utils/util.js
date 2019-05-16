5148b378306bdca8491fc6443bf4396fbf1590f8
raiyen <raiyen@slightech.com>
2019-05-16 16:58:00 +0800

    fix(fix bug): 护照验证修改

--- utils/util.js
+++ utils/util.js
@@ -381,7 +381,8 @@ var QQMapWX = require('qqmap-wx-jssdk.min.js');
   //校验护照
   function checkPassport(passport) {
     if (typeof passport !== 'string') return false;
-    var reg = /^1[45][0-9]{7}|([P|p|S|s]\d{7})|([S|s|G|g]\d{8})|([Gg|Tt|Ss|Ll|Qq|Dd|Aa|Ff]\d{8})|([H|h|M|m]\d{8，10})$/;
+    //var reg = /^1[45][0-9]{7}|([P|p|S|s]\d{7})|([S|s|G|g]\d{8})|([Gg|Tt|Ss|Ll|Qq|Dd|Aa|Ff]\d{8})|([H|h|M|m]\d{8，10})$/;
+    var reg = /^([a-zA-z0-9]){1,20}$/;
     if (reg.test(passport) === false) {
       return false;
     } else {
